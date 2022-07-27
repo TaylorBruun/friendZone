743e6db0b3f87c79c63d91c602849af3bafe424b
TaylorBruun <taylorbruun@gmail.com>
2022-07-27 14:27:43 -0600

    init

---
+++ friendZone.client/src/env.js
@@ -0,0 +1,6 @@
+export const dev = window.location.origin.includes('localhost')
+export const baseURL = dev ? 'https://localhost:5001' : ''
+export const useSockets = false
+export const domain = 'dev-9b4l8l0z.us.auth0.com'
+export const clientId = 'I4Pt8FlULkBPFovOcWdDDZSqO1uDJDQa'
+export const audience = 'https://TayDev.com'
